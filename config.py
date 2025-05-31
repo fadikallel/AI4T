d975f6a0dd3de6deb4111da62dd15c5c0eeb1d6a
davidcombei <102519601+davidcombei@users.noreply.github.com>
2025-05-31 15:40:02 +0300

    Update config.py

--- config.py
+++ config.py
@@ -16,19 +16,19 @@ metadata = [
 ]
 feats_dir = "./feats/wav2vec2-xls-r-2b/"
 feats = [
-        f"wav2vec2-xls-r-2b_Layer10_asv19_train.npy",
-        f"wav2vec2-xls-r-2b_Layer10_asv19_dev.npy",
-        f"wav2vec2-xls-r-2b_Layer10_asv19_eval.npy",
-        f"wav2vec2-xls-r-2b_Layer10_for.npy",
-        f"wav2vec2-xls-r-2b_Layer10_asv21.npy",
-        f"wav2vec2-xls-r-2b_Layer10_asv5_train.npy",
-        f"wav2vec2-xls-r-2b_Layer10_asv5_dev.npy",
-        f"wav2vec2-xls-r-2b_Layer10_odss_PITCH_CROP4.npy",
-        f"wav2vec2-xls-r-2b_Layer10_timit_tts_clean_PITCH_CROP4.npy",
-        f"wav2vec2-xls-r-2b_Layer10_itw.npy",
-        f"wav2vec2-xls-r-2b_Layer10_ai4trust.npy",
-        f"wav2vec2-xls-r-2b_Layer10_mlaad_v5.npy",
-        f"wav2vec2-xls-r-2b_Layer10_m-ailabs_PITCH_CROP4.npy",
+        f"wav2vec2-xls-r-2b_Layer9_asv19_train.npy",
+        f"wav2vec2-xls-r-2b_Layer9_asv19_dev.npy",
+        f"wav2vec2-xls-r-2b_Layer9_asv19_eval.npy",
+        f"wav2vec2-xls-r-2b_Layer9_for.npy",
+        f"wav2vec2-xls-r-2b_Layer9_asv21.npy",
+        f"wav2vec2-xls-r-2b_Layer9_asv5_train.npy",
+        f"wav2vec2-xls-r-2b_Layer9_asv5_dev.npy",
+        f"wav2vec2-xls-r-2b_Layer9_odss_PITCH_CROP4.npy",
+        f"wav2vec2-xls-r-2b_Layer9_timit_tts_clean_PITCH_CROP4.npy",
+        f"wav2vec2-xls-r-2b_Layer9_itw.npy",
+        f"wav2vec2-xls-r-2b_Layer9_ai4trust.npy",
+        f"wav2vec2-xls-r-2b_Layer9_mlaad_v5.npy",
+        f"wav2vec2-xls-r-2b_Layer9_m-ailabs_PITCH_CROP4.npy",
     ]
 metadata_augm = [
             "metadata_marginPruned_XLS_fromALL_margin_both_135.txt",
