ae9e51f8c1d0abadb1762413ef1dd537fb3d9a35
Feidi Kallel <kallel@tu-berlin.de>
2025-09-17 19:25:39 +0200

    based for new ADD

--- config.py
+++ config.py
@@ -1,4 +1,102 @@
-## define the train datasets groups
+DATASETS = {
+    "for": {
+        "indir": "/ds-slt/audio/for-norm/for-norm/",
+        "metadata": "./processed_metadata/for_systems.csv",
+        "outfile": "./feats/wav2vec2-xls-r-2b/wav2vec2-xls-r-2b_Layer9_for.npy"
+    },
+    "timit": {
+        "indir": "/ds-slt/audio/TIMIT/TIMIT-TTS/CLEAN",
+        "metadata": "./processed_metadata/timit_systems.csv",
+        "outfile": "./feats/wav2vec2-xls-r-2b/wav2vec2-xls-r-2b_Layer9_timit.npy"
+    },
+    "asv19_eval": {
+        "indir": "/ds-slt/audio/ASVspoof2019/LA/ASVspoof2019_LA_eval/flac",
+        "metadata": "./processed_metadata/asv19_eval_systems.csv",
+        "outfile": "./feats/wav2vec2-xls-r-2b/wav2vec2-xls-r-2b_Layer9_asv19_eval.npy",
+        "flac": True
+    },
+    "asv19_train": {
+        "indir": "/ds-slt/audio/ASVspoof2019/LA/ASVspoof2019_LA_train/flac",
+        "metadata": "./processed_metadata/asv19_train_systems.csv",
+        "outfile": "./feats/wav2vec2-xls-r-2b/wav2vec2-xls-r-2b_Layer9_asv19_train.npy",
+        "flac": True
+    },
+    "asv19_dev": {
+        "indir": "/ds-slt/audio/ASVspoof2019/LA/ASVspoof2019_LA_dev/flac",
+        "metadata": "./processed_metadata/asv19_dev_systems.csv",
+        "outfile": "./feats/wav2vec2-xls-r-2b/wav2vec2-xls-r-2b_Layer9_asv19_dev.npy",
+        "flac": True
+    },
+    "asv21": {
+        "indir": "/ds-slt/audio/ASVspoof2021/DF/ASVspoof2021_DF_eval/flac",
+        "metadata": "./processed_metadata/asv21_systems.csv",
+        "outfile": "./feats/wav2vec2-xls-r-2b/wav2vec2-xls-r-2b_Layer9_asv21.npy",
+        "flac": True
+    },
+    "asv5_train": {
+        "indir": "/ds-slt/audio/ASVSpoof2024/flac_T/",
+        "metadata": "./processed_metadata/asv5_train_systems.csv",
+        "outfile": "./feats/wav2vec2-xls-r-2b/wav2vec2-xls-r-2b_Layer9_asv5_train.npy",
+        "flac": True
+    },
+    "asv5_dev": {
+        "indir": "/ds-slt/audio/ASVSpoof2024/flac_D/",
+        "metadata": "./processed_metadata/asv5_dev_systems.csv",
+        "outfile": "./feats/wav2vec2-xls-r-2b/wav2vec2-xls-r-2b_Layer9_asv5_dev.npy",
+        "flac": True
+    },
+    "mailabs": {
+        "indir": "/ds-slt/audio/MLAAD/v5/Processed_sr_16000_PCM16/real/",
+        "metadata": "./processed_metadata/mailabs_systems.csv",
+        "outfile": "./feats/wav2vec2-xls-r-2b/wav2vec2-xls-r-2b_Layer9_m-ailabs.npy"
+    },
+    "mlaad_v5": {
+        "indir": "/ds-slt/audio/MLAAD/v5/Processed_sr_16000_PCM16/",
+        "metadata": "./processed_metadata/mlaad_v5_xls_systems.csv",
+        "outfile": "./feats/wav2vec2-xls-r-2b/wav2vec2-xls-r-2b_Layer9_mlaad_v5.npy"
+    },
+    "odss": {
+        "indir": "/ds-slt/audio/ODSS/",
+        "metadata": "./processed_metadata/odss_systems.csv",
+        "outfile": "./feats/wav2vec2-xls-r-2b/wav2vec2-xls-r-2b_Layer9_odss.npy"
+    },
+    "itw": {
+        "indir": "/ds-slt/audio/release_in_the_wild/",
+        "metadata": "./processed_metadata/itw_systems.csv",
+        "outfile": "./feats/wav2vec2-xls-r-2b/wav2vec2-xls-r-2b_Layer9_itw.npy"
+    },
+    "ai4trust": {
+        "indir": "/",
+        "metadata": "./processed_metadata/ai4trust_segm_systems.csv",
+        "outfile": "./feats/wav2vec2-xls-r-2b/wav2vec2-xls-r-2b_Layer9_ai4trust.npy"
+    }, 
+    "ADD22_track1":{
+        "indir": "/ds-slt/audio/yelkheir/ADD22/track1test2/track1test/",
+        "metadata": "./processed_metadata/ADD22_track1.txt",
+        "outfile": "./feats/wav2vec2-xls-r-2b/wav2vec2-xls-r-2b_Layer9_ADD22_track1.npy",
+        "wav": True
+    },
+    "ADD22_track3":{
+        "indir": "/ds-slt/audio/yelkheir/ADD22/track32test/",
+        "metadata": "./processed_metadata/ADD22_track3.txt",
+        "outfile": "./feats/wav2vec2-xls-r-2b/wav2vec2-xls-r-2b_Layer9_ADD22_track3.npy",
+        "wav": True
+    },
+    "ADD2023_round1":{
+        "indir": "/ds/audio/ADD23_track_1.2/Track1.2/testR1/wav/",
+        "metadata": "./processed_metadata/ADD2023_round1.txt",
+        "outfile": "./feats/wav2vec2-xls-r-2b/wav2vec2-xls-r-2b_Layer9_ADD2023_round1.npy",
+        "wav": True
+    },
+    "ADD2023_round2":{
+        "indir": "/ds/audio/ADD23_track_1.2/Track1.2/testR2/wav/",
+        "metadata": "./processed_metadata/ADD2023_round2.txt",
+        "outfile": "./feats/wav2vec2-xls-r-2b/wav2vec2-xls-r-2b_Layer9_ADD2023_round2.npy",
+        "wav": True
+    },
+}
+
+# # define the train datasets groups
 train_groups = {
     "asv19_train": [1],
     "asv19_dev": [2],
--- wav2vec2-xls-r-2b_layer9_extractor.py
+++ wav2vec2-xls-r-2b_layer9_extractor.py
@@ -5,7 +5,8 @@ import numpy as np
 from tqdm import tqdm
 from torch.utils.data import Dataset, DataLoader
 from transformers import AutoFeatureExtractor, Wav2Vec2Model
-
+import parsearg
+from config import DATASETS
 
 class HuggingFaceFeatureExtractor:
     def __init__(self, model_class, name):
@@ -18,7 +19,7 @@ class HuggingFaceFeatureExtractor:
     def __call__(self, audios, srs):
         inputs = self.feature_extractor(
             audios,
-            sampling_rate=srs[0],  # assume all same SR
+            sampling_rate=srs[0],  # assume consistent SR
             return_tensors="pt",
             padding=True,
         )
@@ -29,16 +30,22 @@ class HuggingFaceFeatureExtractor:
 
 
 class AudioDataset(Dataset):
-    def __init__(self, file_list, indir, sr=16000):
+    def __init__(self, file_list, indir, sr=16000, flac=False, wav=False):
         self.file_list = file_list
         self.indir = indir
         self.sr = sr
+        self.flac = flac
+        self.wav = wav
 
     def __len__(self):
         return len(self.file_list)
 
     def __getitem__(self, idx):
         fi = self.file_list[idx]
+        if self.flac:
+            fi = fi + ".flac"
+        if self.wav:
+            fi = fi + ".wav"
         path = os.path.join(self.indir, fi)
         audio, _ = librosa.load(path, sr=self.sr)
         return audio, self.sr, fi
@@ -55,19 +62,21 @@ def read_metadata(file_path):
 
 
 def collate_fn(batch):
-    """Batch list of (audio, sr, filename)."""
     audios, srs, filenames = zip(*batch)
     return list(audios), list(srs), list(filenames)
 
 
-def main(outdir, indir, metadata_file, batch_size=8, num_workers=4):
+def main(config, batch_size=8, num_workers=2):
+    metadata_file = config["metadata"]
     relevant_files = read_metadata(metadata_file)
     print(f"Metadata contains {len(relevant_files)} files.")
+    
     feature_extractor = HuggingFaceFeatureExtractor(
         Wav2Vec2Model, "facebook/wav2vec2-xls-r-2b"
     )
-
-    dataset = AudioDataset(relevant_files, indir, sr=16000)
+    flac = config.get("flac", False)
+    wav = config.get("wav", False)   
+    dataset = AudioDataset(relevant_files, config["indir"], sr=16000, flac=flac, wav=wav)
     dataloader = DataLoader(
         dataset,
         batch_size=batch_size,
@@ -85,16 +94,17 @@ def main(outdir, indir, metadata_file, batch_size=8, num_workers=4):
         layer_embeddings.append(mean_layer_output)
 
     stacked_embeddings = np.vstack(layer_embeddings)
-    os.makedirs(outdir, exist_ok=True)
-    np.save(
-        os.path.join(outdir, "wav2vec2-xls-r-2b_Layer9_mlaad.npy"),
-        stacked_embeddings,
-    )
+    os.makedirs(os.path.dirname(config["outfile"]), exist_ok=True)
+    np.save(config["outfile"], stacked_embeddings)
+
+
 
 
 if __name__ == "__main__":
     print("script running")
-    indir = "/ds-slt/audio/MLAAD/v5/Processed_sr_16000_PCM16/"
-    outdir = "./feats/wav2vec2-xls-r-2b/"
-    metadata_file = "./processed_metadata/mlaad_v5_xls_systems.csv"
-    main(outdir, indir, metadata_file, batch_size=2, num_workers=2)
+    parser = parsearg.ArgumentParser()
+    parser.add_argument("--dataset", type=str, required=True)
+    args = parser.parse_args()
+
+    config = DATASETS[args.dataset]
+    main(config, batch_size=8, num_workers=2)
